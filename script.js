29b393ef840fcb5f398dc018196686ccec0e47ed
Adesewa Aishat <adesewaaishat@gmail.com>
2023-06-08 16:18:07 +0100

    added the color change feature

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const userName = prompt('Hi user, pleaase tell me your name!')
+const userName = prompt('Hi user, please tell me your name!')
 const isConfirmed = confirm('Confirm your name is ' + userName)
 if (isConfirmed){
   alert('welcome to my page ' + userName)
@@ -7,4 +7,11 @@ if (isConfirmed){
 } else {
   alert ('Reload to enter your name again')
 }
-// myFunction(){}
+function myFunction(event){
+  // console.log('event', event);
+  event.preventDefault();
+  const inputValue = event.target.elements.name.value;
+  // alert('That is my favorite color too' + inputValue)
+  const mainElem = document.getElementById('mainContent')
+  mainElem.style.backgroundColor = inputValue;
+};

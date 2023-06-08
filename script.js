4248c4af120c4cdebbfc9bcda58e2dd5b91a3b75
Adesewa Aishat <adesewaaishat@gmail.com>
2023-06-08 14:02:26 +0100

    added new javascript features

---
+++ script.js
@@ -0,0 +1,9 @@
+const userName = prompt('Hi user, pleaase tell m your name!')
+const isConfirmed = confirm('Confirm your name is ' + userName)
+if (isConfirmed){
+  alert('welcome to my page ' + userName)
+  const titleElem =  document.getElementById('contactTitle')
+  titleElem.innerText += ': ' + userName;
+} else {
+  alert ('Reload to enter your name again')
+}
